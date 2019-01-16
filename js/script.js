b983a124c42a355ca233901daeadc48d39a42499
Ilya Kot <scratch18693@gmail.com>
2019-01-16 21:46:51 +0300

    Привел файлы в нужный формат

--- js/script.js
+++ js/script.js
@@ -17,20 +17,20 @@ link.addEventListener("click", function(evt) {
 });
 
 submit.addEventListener("click", function(evt) {
-    if (!datein.value || !dateout.value || !childrens.value || !peoples.value) {
-      evt.preventDefault();
-      popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
-    }
+  if (!datein.value || !dateout.value || !childrens.value || !peoples.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
 });
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-        popup.classList.remove("modal-error");
-      }
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
-  });
+  }
+});
